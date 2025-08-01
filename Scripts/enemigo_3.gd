f7fa725e4bf26fa12161b14d9ca7f391bd585e36
Themecus <127447399+Themecus@users.noreply.github.com>
2025-07-31 21:32:40 -0400

    ADD
    Probemos otra vez

--- Scripts/enemigo_3.gd
+++ Scripts/enemigo_3.gd
@@ -2,41 +2,40 @@ extends CharacterBody2D
 
 @onready var espacio = get_node("/root/Espacio")
 @onready var animacion = $AnimatedSprite2D
-var balaEnemiga=load("res://Scene/bala_enemigo.tscn")
-var vida=4
+var balaEnemiga = load("res://Scene/bala_enemigo.tscn")
+var vida = 4
 var SPEED = 100.0
-var bajar=true
-var tempo=true
+var bajar = true
+var puede_disparar = true  # Reemplaza a "tempo"
 
-func _process(delta):
+func _physics_process(delta):  # Usar physics_process para movimientos
 	moverse(delta)
 
 func moverse(delta):
-	if bajar==false:
-		var bala=balaEnemiga.instantiate()
-		bala.position=Vector2(-0.104, 24.493)
-		position.y += delta * 0
-		if tempo==true:
-			$enfriamiento.start()
-			tempo=false
-			add_child(bala)
-	if bajar==true:
+	if bajar:
 		position.y += delta * SPEED
 		animacion.play("mover")
+	elif puede_disparar:
+		disparar()
+
+func disparar():
+	var bala = balaEnemiga.instantiate()
+	bala.position = Vector2(-0.104, 24.493)  # Ajusta según necesidad
+	add_child(bala)
+	puede_disparar = false
+	$enfriamiento.start()  # Timer para el próximo disparo
 
 func _on_area_2d_area_entered(area):
+	vida -= 1
 	if vida <= 0:
 		espacio.puntuaje.emit(5)
 		queue_free()
-	else:
-		vida-=1
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_movimiento_timeout():
-	bajar=false
-
+	bajar = false
 
 func _on_enfriamiento_timeout():
-	tempo=true
+	puede_disparar = true  # Permite disparar nuevamente
